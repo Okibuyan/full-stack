b971228e24a59b4220a105e0acb951311383b115
Okibuyan <boika.dashdeleg@gmail.com>
2024-10-02 10:44:39 +0800

    back-end-connect

--- front-end/src/app/page.jsx
+++ front-end/src/app/page.jsx
@@ -1,29 +1,38 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Card, CreateModal } from "./components/ui";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const BACKEND_ENDPOINT = "https://full-stack-q1qi.onrender.com";
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(`${BACKEND_ENDPOINT}/products`);
+      const responseData = await response.json();
+      setProducts(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="flex justify-center w-full">
-      \
       <div className="container p-8 flex flex-col gap-5">
         <div className="flex justify-end">
           <CreateModal />
         </div>
         <div className="flex  flex-wrap justify-between gap-6">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {products?.map((product) => {
+            return <Card product={product} />;
+          })}
         </div>
       </div>
     </div>
